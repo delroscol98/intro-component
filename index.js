17caa1a5611761ab698d97b5496ff60424b99e01
delroscol98 <collindelrosario98@gmail.com>
2024-05-30 00:11:11 +1000

    JS modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
-const passwork = document.getElementById("password");
+const password = document.getElementById("password");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -27,9 +27,38 @@ const validateFirstName = () => {
   if (value === "") {
     errorIcon.classList.add("invalid");
     errorMsg.classList.add("invalid");
+  } else {
+    errorIcon.classList.remove("invalid");
+    errorMsg.classList.remove("invalid");
+  }
+};
+
+const validateLastName = () => {
+  const [errorMsg, errorIcon] = getErrorIconAndMsg(lastName);
+  const value = lastName.value.trim();
+  if (value === "") {
+    errorIcon.classList.add("invalid");
+    errorMsg.classList.add("invalid");
+  } else {
+    errorIcon.classList.remove("invalid");
+    errorMsg.classList.remove("invalid");
+  }
+};
+
+const validatePassword = () => {
+  const [errorMsg, errorIcon] = getErrorIconAndMsg(password);
+  const value = password.value.trim();
+  if (value === "") {
+    errorIcon.classList.add("invalid");
+    errorMsg.classList.add("invalid");
+  } else {
+    errorIcon.classList.remove("invalid");
+    errorMsg.classList.remove("invalid");
   }
 };
 
 const validateForm = () => {
   validateFirstName();
+  validateLastName();
+  validatePassword();
 };
